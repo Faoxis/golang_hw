15b89f6dc449660c9bcc65deea3c65342f9c3242
Sergei <Faoxis@mail.ru>
2025-01-26 13:38:06 +0300

    Hw05 parallel execution (#5)
    homework with parallel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,75 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+	// Создаем канал для задач
+	taskChannel := make(chan Task, n)
+
+	// Создаем канал для сигнала завершения
+	doneChannel := make(chan struct{})
+	onceForDoneChannel := sync.Once{}
+	closeDoneChannel := func() {
+		onceForDoneChannel.Do(func() {
+			close(doneChannel)
+		})
+	}
+
+	wg := sync.WaitGroup{}
+	errorCounter := atomic.Int64{}
+
+	worker := func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-doneChannel:
+				return
+			case task, ok := <-taskChannel:
+				if !ok {
+					return
+				}
+
+				err := task()
+				if err != nil {
+					errorCounter.Add(1)
+					if errorCounter.Load() >= int64(m) {
+						closeDoneChannel()
+						return
+					}
+				}
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker()
+	}
+
+	go func() {
+		defer close(taskChannel)
+		i := 0
+		for i < len(tasks) && errorCounter.Load() < int64(m) {
+			select {
+			case <-doneChannel:
+				return
+			default:
+			}
+
+			select {
+			case <-doneChannel:
+				return
+			case taskChannel <- tasks[i]:
+			}
+			i++
+		}
+	}()
+
+	wg.Wait()
+	closeDoneChannel()
+	if errorCounter.Load() >= int64(m) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -15,6 +15,45 @@ import (
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
+	t.Run("tasks without errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		goroutinsCounter := atomic.Int32{}
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				goroutinsCounter.Add(1)
+				defer goroutinsCounter.Add(-1)
+
+				time.Sleep(time.Millisecond)
+				atomic.AddInt32(&runTasksCount, 1)
+
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		resChan := make(chan error)
+		defer close(resChan)
+
+		go func() {
+			err := Run(tasks, workersCount, maxErrorsCount)
+			resChan <- err
+		}()
+		require.Eventually(t, func() bool {
+			return int32(workersCount) == goroutinsCounter.Load()
+		}, time.Second*2, time.Millisecond)
+
+		err := <-resChan
+		require.NoError(t, err)
+
+		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
+	})
+
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -38,33 +77,33 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
+	t.Run("if task list is empty", func(t *testing.T) {
+		err := Run([]Task{}, 10, 1)
+		require.Nil(t, err)
+	})
 
-		var runTasksCount int32
-		var sumTime time.Duration
+	t.Run("if goroutins more then tasks", func(t *testing.T) {
+		taskCounter := 50
+		goroutinCounter := 100
 
-		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
-
-			tasks = append(tasks, func() error {
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
+		tasks := make([]Task, 0, taskCounter)
+		for i := 0; i < 0; i++ {
+			tasks[i] = func() error {
+				time.Sleep(time.Millisecond * 10)
 				return nil
-			})
+			}
 		}
+		err := Run(tasks, goroutinCounter, 1)
+		require.Nil(t, err)
+	})
 
-		workersCount := 5
-		maxErrorsCount := 1
+	t.Run("if m is zero", func(t *testing.T) {
+		err := Run([]Task{}, 1, 0)
+		require.ErrorIs(t, ErrErrorsLimitExceeded, err)
+	})
 
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
-
-		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	t.Run("if m is negative", func(t *testing.T) {
+		err := Run([]Task{}, 1, -42)
+		require.ErrorIs(t, ErrErrorsLimitExceeded, err)
 	})
 }
