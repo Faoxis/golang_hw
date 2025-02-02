c986b90c9dccf68dd7f2a67ca3e32af63c7253ec
Sergei <Faoxis@mail.ru>
2025-02-02 19:49:46 +0300

    adding solution (#6)
    * adding solution
    
    * adding tests
    
    * fixing done case
    
    ---------
    
    Co-authored-by: Samoilov Sergei <sgsamoilov@mts.ru>

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,43 @@ type (
 
 type Stage func(in In) (out Out)
 
+func createProxyChannel(in In, done In) Out {
+	proxyChannel := make(Bi)
+	go func() {
+		defer close(proxyChannel)
+		for {
+			select {
+			case <-done:
+				return
+			case task, ok := <-in:
+				if !ok {
+					return
+				}
+				if task != nil {
+					select {
+					case <-done:
+						return
+					case proxyChannel <- task:
+					}
+				}
+			}
+		}
+	}()
+	return proxyChannel
+}
+
+func stageWrapper(in In, done In, stage Stage) Out {
+	if done != nil {
+		proxyChannel := createProxyChannel(in, done)
+		return stage(proxyChannel)
+	}
+	return stage(in)
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stageWrapper(out, done, stage)
+	}
+	return createProxyChannel(out, done)
 }
