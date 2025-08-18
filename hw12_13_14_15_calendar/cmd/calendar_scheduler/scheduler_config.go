74c35af03a2cb188fc0f9e9c9718253e88a6c230
Сергей Самойлов <faoxis@MacBook-Pro-Sergej.local>
2025-08-18 13:07:13 +0300

    starting

---
+++ hw12_13_14_15_calendar/cmd/calendar_scheduler/scheduler_config.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	yml "gopkg.in/yaml.v3"
+)
+
+type SchedulerConfig struct {
+	Logger  Logger
+	Storage Storage
+}
+
+type Storage struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+	SSLMode  string `yaml:"sslmode"`
+}
+
+type Logger struct {
+	Level string
+}
+
+func LoadConfig(path string) (*SchedulerConfig, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open config file: %w", err)
+	}
+	defer file.Close()
+
+	decoder := yml.NewDecoder(file)
+	var cfg SchedulerConfig
+	if err := decoder.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to decode config file: %w", err)
+	}
+	return &cfg, nil
+}
+
+func (storage *Storage) GetPostgresDSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		storage.Host,
+		storage.Port,
+		storage.User,
+		storage.Password,
+		storage.Database,
+		storage.SSLMode,
+	)
+}
