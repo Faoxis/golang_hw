efb5c7a46241bd3ed754c70fe56f29c47d78ca11
Сергей Самойлов <faoxis@MacBook-Pro-Sergej.local>
2025-09-18 17:45:45 +0300

    Clean up project for HW16 - remove temporary files

--- hw12_13_14_15_calendar/env/start-calendar.sh
+++ hw12_13_14_15_calendar/env/start-calendar.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Подставляем переменные окружения в конфигурацию
+envsubst < ./configs/config.yml > ./configs/config-resolved.yml
+
 # Запускаем приложение
-exec ./calendar -config ./configs/config.yml
+exec ./calendar -config ./configs/config-resolved.yml
